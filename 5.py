084c7584b545c0c1ce266fec43e36dff11274f5d
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-10-14 12:51:24 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,16 +1,7 @@
-x = float(input("Введите координату x: "))
-y = float(input("Введите координату y: "))
-if x > 0 and y > 0:
-    print("Точка лежит в первой четверти")
-elif x > 0 and y < 0:
-    print("Точка лежит в четвёртой четверти")
-elif x < 0 and y < 0:
-    print("Точка лежит в третьей четверти")
-elif x < 0 and y > 0:
-    print("Точка лежит во второй четверти")
-elif x == 0 and y != 0:
-    print("Точка лежит на оси x")
-elif x != 0 and y == 0:
-    print("Точка лежит на оси y")
-else:
-    print("Точка находится в начале координат")
+from collections import Counter
+from task1 import enter
+
+print('Элемент | Частота')
+els = enter(input())
+ans = Counter(els)
+print(*[f'{i} | {ans[i]}' for i in ans], sep='\n')
